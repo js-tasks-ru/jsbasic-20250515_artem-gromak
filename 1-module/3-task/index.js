65eaa5890a138972ce0e4f65ac171f6554af1a2d
m01eky1a <gromack2017@yandex.ru>
2025-05-20 00:12:06 +0300

    1_lesson_task_3

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,11 @@
 function ucFirst(str) {
-  // ваш код...
+  if (typeof str !== "string") {
+    throw new TypeError("Аргумент должен быть строкой.");
+  }
+
+  if (!str) {
+    return str;
+  }
+
+  return str[0].toUpperCase() + str.slice(1);
 }
