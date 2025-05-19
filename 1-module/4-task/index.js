75094844faf8c91ea73a37dfa2b5df26f9824bf9
m01eky1a <gromack2017@yandex.ru>
2025-05-20 00:12:11 +0300

    1_lesson_task_4

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,11 @@
 function checkSpam(str) {
-  // ваш код...
+  if (typeof str !== "string") {
+    throw new TypeError("Аргумент должен быть строкой.");
+  }
+  if (!str) {
+    return false;
+  }
+
+  const lowerStr = str.toLowerCase();
+  return lowerStr.includes("1xbet") || lowerStr.includes("xxx");
 }
