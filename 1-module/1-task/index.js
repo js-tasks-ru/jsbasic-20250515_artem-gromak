36f426e01f0fe79c35961794164c7e3e148921fa
m01eky1a <gromack2017@yandex.ru>
2025-05-19 23:15:20 +0300

    1_lesson_task_1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,21 @@
-function factorial(n) {
-  // ваш код...
+function factorial(num) {
+  if (typeof num !== "number") {
+    throw new TypeError("Аргумент должнен быть числом");
+  }
+
+  if (num < 0) {
+    throw new TypeError("Аргемент не должен быть меньше нуля");
+  }
+
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+
+  let result = 1;
+
+  for (let i = 2; i <= num; i++) {
+    result *= i;
+  }
+
+  return result;
 }
