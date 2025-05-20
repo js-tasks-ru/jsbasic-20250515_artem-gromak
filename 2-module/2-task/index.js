6819a9b4c891bed0d428e6de63ddde718f83eb10
m01eky1a <gromack2017@yandex.ru>
2025-05-20 23:27:15 +0300

    2_lesson_task_2

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,13 @@
 function isEmpty(obj) {
-  // ваш код...
+  if (typeof obj !== "object") {
+    throw new TypeError("Аргумент должнен быть объектом");
+  }
+
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      return false;
+    }
+  }
+
+  return true;
 }
