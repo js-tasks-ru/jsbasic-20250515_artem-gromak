48c1e88a0b2abf48fac79e8bec4c75d87f94cf5b
m01eky1a <gromack2017@yandex.ru>
2025-05-22 23:27:19 +0300

    3_lesson_task_5

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,13 @@
 function getMinMax(str) {
-  // ваш код...
+  // Проверка, что str — строка
+  if (typeof str !== "string") {
+    throw new Error("Аргумент должен быть строкой");
+  }
+  // Получаем массив и фультруем в нем числа
+  const numbers = str.split(" ").filter((el) => Number(el));
+
+  return {
+    min: Math.min(...numbers),
+    max: Math.max(...numbers),
+  };
 }
