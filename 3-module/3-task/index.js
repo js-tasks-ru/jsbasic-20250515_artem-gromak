aceabe8b055540897bb2e09bbec07db8f287f5ef
m01eky1a <gromack2017@yandex.ru>
2025-05-22 23:27:13 +0300

    3_lesson_task_3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,13 @@
 function camelize(str) {
-  // ваш код...
+  // Проверка, что str — строка
+  if (typeof str !== "string") {
+    throw new Error("Аргумент должен быть строкой");
+  }
+
+  return str
+    .split("-")
+    .map((el, idx) => {
+      return idx == 0 ? el : el.charAt(0).toUpperCase() + el.slice(1);
+    })
+    .join("");
 }
