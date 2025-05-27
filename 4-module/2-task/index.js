d86dbca48da1918c8592cb09c1fb0506c9fb4ac9
m01eky1a <gromack2017@yandex.ru>
2025-05-28 00:46:29 +0300

    4_lesson_task_2

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,12 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  // Проверка, что table — это DOM-элемент <table>
+  if (!(table instanceof HTMLTableElement)) {
+    throw new TypeError("Аргумент должен быть элементом <table>");
+  }
+
+  for (let i = 0; i < table.rows.length; i++) {
+    if (table.rows[i].cells[i]) {
+      table.rows[i].cells[i].style.backgroundColor = "red";
+    }
+  }
 }
