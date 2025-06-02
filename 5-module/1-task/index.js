a7636a2c2e201ca014d351f961330e09d2bf6b8c
m01eky1a <gromack2017@yandex.ru>
2025-06-02 22:33:46 +0300

    4_lesson_task_1

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,15 @@
 function hideSelf() {
-  // ваш код...
+  // Находим кнопку с классом hide-self-button
+  const button = document.querySelector(".hide-self-button");
+
+  // Проверяем, что кнопка существует
+  if (!button) {
+    console.warn("Кнопка с классом hide-self-button не найдена");
+    return;
+  }
+
+  // Добавляем обработчик события click
+  button.addEventListener("click", () => {
+    button.setAttribute("hidden", "");
+  });
 }
